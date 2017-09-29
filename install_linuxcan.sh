dcd2c857dbbd5e7bef42c05396a25f1e2520388a
Joshua Whitley <jwhitley@autonomoustuff.com>
2017-09-29 01:28:23 -0500

    Still trying.

---
+++ install_linuxcan.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -ex
+
+CANLIB_LIBNAME=libcanlib.so
+CANLIB_SONAME=libcanlib.so.1
+CANLIB_LIBRARY=libcanlib.so.1.2.0
+
+LINLIB_LIBNAME=liblinlib.so
+LINLIB_SONAME=liblinlib.so.1
+LINLIB_LIBRARY=liblinlib.so.1.2.0
+
+cd /tmp
+wget http://www.kvaser.com/software/7330130980754/V5_20_0/linuxcan.tar.gz
+tar xvf linuxcan.tar.gz
+cd /tmp/linuxcan
+make canlib linlib
+
+cd /tmp/linuxcan/canlib
+make -C examples install
+rm -f /usr/lib/$CANLIB_LIBNAME
+rm -f /usr/lib/$CANLIB_SONAME
+install  -m 644 $CANLIB_LIBRARY /usr/lib/
+ln -sf $CANLIB_LIBRARY /usr/lib/$CANLIB_LIBNAME
+ln -sf $CANLIB_LIBRARY /usr/lib/$CANLIB_SONAME
+/sbin/ldconfig
+install -m 644 ../include/canlib.h /usr/include
+install -m 644 ../include/canstat.h /usr/include
+install -m 644 ../include/obsolete.h /usr/include
+
+cd /tmp/linuxcan/linlib
+rm -f /usr/lib/$(LINLIB_LIBNAME)
+rm -f /usr/lib/$(LINLIB_SONAME)
+install  -m 644 $(LINLIB_LIBRARY) /usr/lib/
+ln -sf $(LINLIB_LIBRARY) /usr/lib/$(LINLIB_LIBNAME)
+ln -sf $(LINLIB_LIBRARY) /usr/lib/$(LINLIB_SONAME)
+/sbin/ldconfig -X
+install -m 644 ../include/linlib.h /usr/include
