4fac599a5a4382cd573c3508bf2eb8106da4970c
Josh Whitley <jwhitley@autonomoustuff.com>
2017-11-13 22:49:29 -0600

    Re-releasing under MIT license.

--- src/kvaser_can_bridge.cpp
+++ src/kvaser_can_bridge.cpp
@@ -1,18 +1,8 @@
 /*
-* AutonomouStuff, LLC ("COMPANY") CONFIDENTIAL
-* Unpublished Copyright (c) 2009-2016 AutonomouStuff, LLC, All Rights Reserved.
+* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
 *
-* NOTICE:  All information contained herein is, and remains the property of COMPANY. The intellectual and technical concepts contained
-* herein are proprietary to COMPANY and may be covered by U.S. and Foreign Patents, patents in process, and are protected by trade secret or copyright law.
-* Dissemination of this information or reproduction of this material is strictly forbidden unless prior written permission is obtained
-* from COMPANY.  Access to the source code contained herein is hereby forbidden to anyone except current COMPANY employees, managers or contractors who have executed
-* Confidentiality and Non-disclosure agreements explicitly covering such access.
-*
-* The copyright notice above does not evidence any actual or intended publication or disclosure  of  this source code, which includes
-* information that is confidential and/or proprietary, and is a trade secret, of  COMPANY.   ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC  PERFORMANCE,
-* OR PUBLIC DISPLAY OF OR THROUGH USE  OF THIS  SOURCE CODE  WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE
-* LAWS AND INTERNATIONAL TREATIES.  THE RECEIPT OR POSSESSION OF  THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS
-* TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT  MAY DESCRIBE, IN WHOLE OR IN PART.
+* This file is part of the Kvaser ROS 1.0 driver which is released under the MIT license.
+* See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
 */
 
 #include <thread>
