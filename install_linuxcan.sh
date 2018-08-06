a216f0a1f9f02ebb11a86a2c86b3635e4274049d
Joshua Whitley <jwhitley@autonomoustuff.com>
2018-08-06 18:06:59 -0500

    Fixing PPA URL in build script.

--- install_linuxcan.sh
+++ install_linuxcan.sh
@@ -2,6 +2,6 @@
 set -ex
 
 apt-get install -y software-properties-common linux-image-generic linux-headers-generic
-apt-add-repository -y ppa:jwhitleyastuff/kvaser-linuxcan-dkms
+apt-add-repository -y ppa:jwhitleyastuff/linuxcan-dkms
 apt-get update
 apt-get install -y linuxcan-dkms
