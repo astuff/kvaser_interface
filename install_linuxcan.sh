781139d11f92f7edfcefe4d9365cc912178b0018
Joshua Whitley <jwhitley@autonomoustuff.com>
2018-08-06 18:18:21 -0500

    Seeing if we can get away with just the kernel headers.

--- install_linuxcan.sh
+++ install_linuxcan.sh
@@ -4,4 +4,4 @@ set -ex
 apt-get install -y software-properties-common
 apt-add-repository -y ppa:jwhitleyastuff/linuxcan-dkms
 apt-get update
-apt-get install -y linux-image-generic linux-headers-generic linuxcan-dkms
+apt-get install -y linux-headers-generic linuxcan-dkms
