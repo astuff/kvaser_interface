6f413f21ea93c627348e03f95b7e9abd0537a918
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-01-23 15:05:06 -0600

    Revert "Revert "Memory Management Improvements and Clean-up""
    This reverts commit ce101fa2520fd12155d59dafdcfb80bdfdd67759.

--- include/kvaser_interface/kvaser_interface.h
+++ include/kvaser_interface/kvaser_interface.h
@@ -1,78 +1,81 @@
 /*
-* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
+* Unpublished Copyright (c) 2009-2019 AutonomouStuff, LLC, All Rights Reserved.
 *
-* This file is part of the Kvaser ROS 1.0 driver which is released under the MIT license.
+* This file is part of the Kvaser ROS driver which is released under the MIT license.
 * See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
 */
 
-// Define a class that supports a basic CAN interface that's independent of the hardware and driver library used
-// Different libraries can be created to define all these functions for a specific driver library
+#ifndef KVASER_INTERFACE_KVASER_INTERFACE_H
+#define KVASER_INTERFACE_KVASER_INTERFACE_H
 
-#ifndef KVASER_INTERFACE_HPP
-#define KVASER_INTERFACE_HPP
-
-//C++ Includes
+// C++ Includes
 #include <iostream>
+#include <memory>
 
-//OS Includes
-#include <unistd.h>
+extern "C"
+{
+#include <canlib.h>
+}
 
 namespace AS
 {
 namespace CAN
 {
-  enum return_statuses
-  {
-    OK = 0,
-    INIT_FAILED = -1,
-    BAD_PARAM = -2,
-    NO_CHANNELS_FOUND = -3,
-    CHANNEL_CLOSED = -4,
-    NO_MESSAGES_RECEIVED = -5,
-    READ_FAILED = -6,
-    WRITE_FAILED = -7,
-    CLOSE_FAILED = -8
-  };
 
-  class KvaserCan
-  {
+enum ReturnStatuses
+{
+  OK = 0,
+  INIT_FAILED = -1,
+  BAD_PARAM = -2,
+  NO_CHANNELS_FOUND = -3,
+  CHANNEL_CLOSED = -4,
+  NO_MESSAGES_RECEIVED = -5,
+  READ_FAILED = -6,
+  WRITE_FAILED = -7,
+  CLOSE_FAILED = -8
+};
+
+class KvaserCan
+{
   public:
     KvaserCan();
 
     ~KvaserCan();
 
     // Called to pass in parameters and open can link
-    return_statuses open(const int& hardware_id,
-                         const int& circuit_id,
-                         const int& bitrate,
-                         const bool& echo_on = true);
+    ReturnStatuses open(const int32_t& hardware_id,
+                        const int32_t& circuit_id,
+                        const int32_t& bitrate,
+                        const bool& echo_on = true);
 
     // Close the can link
-    return_statuses close();
+    ReturnStatuses close();
 
     // Check to see if the CAN link is open
-    bool is_open();
+    bool isOpen();
 
     // Read a message
-    return_statuses read(long *id,
-                         unsigned char *msg,
-                         unsigned int *size,
-                         bool *extended,
-                         unsigned long *time);
+    ReturnStatuses read(int64_t *id,
+                        uint8_t *msg,
+                        uint32_t *size,
+                        bool *extended,
+                        uint64_t *time);
 
     // Send a message
-    return_statuses write(const long& id,
-                          unsigned char *msg,
-                          const unsigned int& size,
-                          const bool& extended);
+    ReturnStatuses write(const int64_t& id,
+                         uint8_t *msg,
+                         const uint32_t& size,
+                         const bool& extended);
 
   private:
-    void *handle;
+    std::unique_ptr<CanHandle> handle;
     bool on_bus;
-  };
+};
 
-  // Converts error messages to human-readable strings
-  std::string return_status_desc(const return_statuses& ret);
-}
-}
-#endif
+// Converts error messages to human-readable strings
+std::string returnStatusDesc(const ReturnStatuses& ret);
+
+}  // namespace CAN
+}  // namespace AS
+
+#endif  // KVASER_INTERFACE_KVASER_INTERFACE_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,48 +1,32 @@
 /*
-* Unpublished Copyright (c) 2009-2017 AutonomouStuff, LLC, All Rights Reserved.
+* Unpublished Copyright (c) 2009-2019 AutonomouStuff, LLC, All Rights Reserved.
 *
 * This file is part of the Kvaser ROS 1.0 driver which is released under the MIT license.
 * See file LICENSE included with this software or go to https://opensource.org/licenses/MIT for full license details.
 */
 
-#include <kvaser_interface.h>
+#include <kvaser_interface/kvaser_interface.h>
 
-std::string AS::CAN::return_status_desc(const return_statuses& ret)
+std::string AS::CAN::returnStatusDesc(const ReturnStatuses& ret)
 {
   std::string status_string;
 
   if (ret == INIT_FAILED)
-  {
     status_string = "Initialization of the CAN interface failed.";
-  }
   else if (ret == BAD_PARAM)
-  {
     status_string = "A bad parameter was provided to the CAN interface during initalization.";
-  }
   else if (ret == NO_CHANNELS_FOUND)
-  {
     status_string = "No available CAN channels were found.";
-  }
   else if (ret == CHANNEL_CLOSED)
-  {
     status_string = "CAN channel is not currently open.";
-  }
   else if (ret == NO_MESSAGES_RECEIVED)
-  {
     status_string = "No messages were received on the interface.";
-  }
   else if (ret == READ_FAILED)
-  {
     status_string = "A read operation failed on the CAN interface.";
-  }
   else if (ret == WRITE_FAILED)
-  {
     status_string = "A write operation failed on the CAN interface.";
-  }
   else if (ret == CLOSE_FAILED)
-  {
     status_string = "Closing the CAN interface failed.";
-  }
 
   return status_string;
 }
